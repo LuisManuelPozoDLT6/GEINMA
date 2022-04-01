cd90d7e0f7bbdffd69f457857d62ea4bc468ee6a
LuisManuelPozoDLT6 <20203tn047@utez.edu.mx>
2022-04-01 03:14:18 -0600

    commit 01/04/22

--- GEINMA/GEINMA/src/main/java/mx/edu/utez/GEINMA/incedence/controller/IncidenceController.java
+++ GEINMA/GEINMA/src/main/java/mx/edu/utez/GEINMA/incedence/controller/IncidenceController.java
@@ -7,30 +7,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 
 @RestController
 @RequestMapping("/incidence")
 @CrossOrigin(origins = {"*"})
 public class IncidenceController {
+    SimpleDateFormat formato = new SimpleDateFormat("dd 'de' MMMM 'del' yyyy", new Locale("es_ES"));
     @Autowired
     IncidenceService incidenceService;
 
     @GetMapping("/")
     public ResponseEntity<Message> getAll(){return incidenceService.findAll();}
 
-    @GetMapping("client/{client}")
+    @GetMapping("/client/{client}")
     public ResponseEntity<Message> getByClient(@PathVariable("client") long id){
         return incidenceService.findAllByClient(id);
     }
 
-    @GetMapping("technical/{technical}")
+    @GetMapping("/technical/{technical}")
     public ResponseEntity<Message> getByTechnical(@PathVariable("technical") long id){
         return incidenceService.findAllByTechnical(id);
     }
 
-    @GetMapping("status/{status}")
+    @GetMapping("/status/{status}")
     public ResponseEntity<Message> getByStatus(@PathVariable("status") long id){
         return incidenceService.findAllByStatus(id);
     }
@@ -42,12 +45,12 @@ public class IncidenceController {
 
     @PostMapping("/")
     public ResponseEntity<Message> save(@RequestBody IncidenceDTO incidenceDTO){
-        return incidenceService.save(new Incidence(incidenceDTO.getDescription(), new Date().toString(), incidenceDTO.getClient(), incidenceDTO.getTechnical(), incidenceDTO.getStatus(), incidenceDTO.getExperience(), incidenceDTO.getPictureIncidence()));
+        return incidenceService.save(new Incidence(incidenceDTO.getDescription(), formato.format(new Date()), incidenceDTO.getClient(), incidenceDTO.getTechnical(), incidenceDTO.getStatus(), incidenceDTO.getExperience(), incidenceDTO.getPictureIncidence()));
     }
 
     @PutMapping("/")
     public ResponseEntity<Message> update(@RequestBody IncidenceDTO incidenceDTO){
-        return incidenceService.save(new Incidence(incidenceDTO.getId(), incidenceDTO.getDescription(), new Date().toString(), incidenceDTO.getClient(), incidenceDTO.getTechnical(), incidenceDTO.getStatus(), incidenceDTO.getExperience(), incidenceDTO.getPictureIncidence()));
+        return incidenceService.save(new Incidence(incidenceDTO.getId(), incidenceDTO.getDescription(), formato.format(new Date()), incidenceDTO.getClient(), incidenceDTO.getTechnical(), incidenceDTO.getStatus(), incidenceDTO.getExperience(), incidenceDTO.getPictureIncidence()));
     }
 
 
